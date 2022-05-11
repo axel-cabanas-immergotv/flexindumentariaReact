50d2da1a6c8708537e372eca3d14db47a314a4d3
unknown <axelca72@gmail.com>
2022-05-11 19:45:52 -0300

    agregar sesion item sin stock

--- src/components/ItemCount/ItemCount.js
+++ src/components/ItemCount/ItemCount.js
@@ -1,5 +1,4 @@
 import React from "react"
-
 export default function ItemCount ({stock}){
     const [count, setCount] = React.useState(0);
     const handdleAdd = () => {
@@ -7,21 +6,28 @@ export default function ItemCount ({stock}){
             setCount(count + 1);
         };
     }
-
     const handdleRemove = () => {
-        if(count === 0) {
-            setCount(count + 0);
-        }
-         else {
-            setCount(count - 1)
+        if(count > 0) {
+            setCount(count - 1);
         }
     }
-
+    
     return (
         <div className="banner__btn-container d-flex">
-            <button className="banner__link-btn px-2" onClick={handdleAdd}>+</button>
-            <p>{count}</p>
-            <button className="banner__link-btn px-2" onClick={handdleRemove}>-</button>
+            {
+                stock === 0
+                ? (
+                    <p>Sin stock</p> 
+                )
+                : ( 
+                    <>
+                        <button className="banner__link-btn px-2" onClick={handdleAdd}>+</button>
+                        <p>{count}</p>
+                        <button className="banner__link-btn px-2" onClick={handdleRemove}>-</button>
+                    </> 
+                )
+            }
         </div>
     )
+    
 }
